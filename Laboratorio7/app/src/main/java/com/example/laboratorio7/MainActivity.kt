dc44dca872949242f190365f719a7453ce4d1c50
adrianap0607 <adrianap2670@gmail.com>
2024-08-25 18:27:09 -0600

    Correccion lab7

--- Laboratorio7/app/src/main/java/com/example/laboratorio7/MainActivity.kt
+++ Laboratorio7/app/src/main/java/com/example/laboratorio7/MainActivity.kt
@@ -7,13 +7,14 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import com.example.laboratorio7.Lab7.NotificationCenter
 import androidx.compose.material3.MaterialTheme
+import com.example.laboratorio7.ui.theme.Laboratorio7Theme
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            Laboratorio7Theme  {
                 NotificationCenter() // Asegúrate de que esta función esté correctamente importada
             }
         }
